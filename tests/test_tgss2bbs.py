c8bca30d7966ce4a8dbeb435025e1102c1a37a4b
Alexandar Mechev <apmechev@gmail.com>
2019-02-19 09:52:12 +0100

    Adding test for tgss2bbs.main()

---
+++ tests/test_tgss2bbs.py
@@ -0,0 +1,14 @@
+from tempfile import NamedTemporaryFile
+from tgss2bbs2 import  main 
+
+
+def test_makes_a_skymodel_from_3Csource():
+    with NamedTemporaryFile() as temp_file:
+        main(srcID="3C48", radius=1, DoDec=True, output=temp_file.name)
+        lines=[]
+        for line in open(temp_file,'r').read().split('\n'):
+            lines.append(line)
+        assert lines[0] == "FORMAT = Name, Type, Patch, Ra, Dec, I, Q, U, V, MajorAxis, MinorAxis, Orientation, ReferenceFrequency='147500000.0', SpectralIndex='[]'"
+        assert lines[2] ==" , , Patch, 01:37:41.2996, +33.09.35.079"
+        for line in lines[3:]:
+            assert "GAUSSIAN" in line or "POINT" in line
