3c89609f86d51d493c6a4865a63f960d46466ea5
Alexandar Mechev <apmechev@gmail.com>
2019-02-19 10:11:28 +0100

    using temp_file.name to open for reading and removing python 2.6 and 3.3 since it doesn't work with astropy

--- tests/test_tgss2bbs.py
+++ tests/test_tgss2bbs.py
@@ -6,7 +6,7 @@ def test_makes_a_skymodel_from_3Csource():
     with NamedTemporaryFile() as temp_file:
         main(srcID="3C48", radius=1, DoDec=True, output=temp_file.name)
         lines=[]
-        for line in open(temp_file,'r').read().split('\n'):
+        for line in open(temp_file.name,'r').read().split('\n'):
             lines.append(line)
         assert lines[0] == "FORMAT = Name, Type, Patch, Ra, Dec, I, Q, U, V, MajorAxis, MinorAxis, Orientation, ReferenceFrequency='147500000.0', SpectralIndex='[]'"
         assert lines[2] ==" , , Patch, 01:37:41.2996, +33.09.35.079"
