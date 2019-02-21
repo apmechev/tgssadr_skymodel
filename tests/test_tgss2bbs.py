05df83e928810c40e3406e5c7ac350f1d77af91c
Alexandar Mechev <apmechev@gmail.com>
2019-02-21 16:39:37 +0100

    Adding test for 3C48 both using its name and coordinates

--- tests/test_tgss2bbs.py
+++ tests/test_tgss2bbs.py
@@ -12,3 +12,23 @@ def test_makes_a_skymodel_from_3Csource():
         assert lines[2] ==" , , Patch, 01:37:41.2996, +33.09.35.079"
         for line in lines[3:-2]:
             assert "GAUSSIAN" in line or "POINT" in line
+
+def test_name_and_coord_equals():
+    coords = "24.42208167, 33.15974417"
+    name = "3C48"
+    with NamedTemporaryFile() as name_model:
+        with NamedTemporaryFile() as coords_model:
+            main(srcID=name, radius=1, DoDec=True, output=name_model.name)
+            main(srcID=coords, radius=1, DoDec=True, output=coords_model.name)
+            with open(name_model.name,'r') as _f:
+                name_data = _f.read().split('\n')
+                line_count_name = len(name_data)
+            with open(coords_model.name,'r') as _f:
+                coord_data = _f.read().split('\n')
+                line_count_coord = len(coord_data)
+            assert line_count_name == line_count_coord
+
+            for line_n, line_c in zip(name_data, coord_data):
+                assert line_n == line_c
+
+
